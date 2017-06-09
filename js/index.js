ec3f7f40612e702617d0fe2c06d26b13c8006266
chen guo <865306197@qq.com>
2017-06-09 21:16:12 +0800

    update

--- js/index.js
+++ js/index.js
@@ -2,17 +2,5 @@
  * Created by fruit on 17/1/4.
  */
 window.onload=function(){
-    var menu=document.getElementById("menu");
-    var list=menu.getElementsByTagName("a");
-    var str=location.href;
-    var arr=str.split("/");
-    switch (arr[arr.length-1]){
-        case "works":
-            list[1].style.color="yellow";
-            break;
-        case "about":
-            list[2].style.color="yellow";
-            break;
-        default:list[0].style.color="yellow";
-    }
+
 };
